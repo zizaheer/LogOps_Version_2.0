c06f6411e0a110b4e1dbe0cdf0822aefd55569ab
Kazi Islam <zizaheer@hotmail.com>
2020-02-26 17:56:09 -0500

    ok

--- LogOps_Version_2.0/wwwroot/js/site.js
+++ LogOps_Version_2.0/wwwroot/js/site.js
@@ -2,3 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+//Dispatch board starts
+
+$('#btn-add-driver').on('click', function () {
+    
+    $('.driver-container').prepend($('.driver-block')[0].outerHTML);
+    $('.content-body .driver-block').removeClass('hide-block');
+});
+
+function toggleDriverCheckbox() {
+    $(".chk-select-driver").change(function () {
+        $(".chk-select-driver").not(this).prop('checked', false);
+    });
+}
+//Dispatch board finishes
